ce2f99ad317bbab19725c982019a15ad8ad42052
上海－郭朝阳 <guochaoyang@jd.com>
2016-01-11 21:49:19 +0800

    Initial Commit

---
+++ ShoppingCart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingCart
+//
+//  Created by ccguo on 16/1/11.
+//  Copyright © 2016年 ccguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
